9eb51854b97c1143423e19864d72a9ac4693beab
dennnoval <dennnoval@gmail.com>
2021-05-06 19:07:23 +0700

    Update files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React from 'react'
 import './App.css'
 import logo512 from './res/logo512.png'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+// Action
+const BUY_CAKE = 'BUY_CAKE'
+const buyCake = () => { type: BUY_CAKE }
+
+// State
+const initialState = {
+  numOfCakes: 10
+}
+
+// Reducer
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE: return { ...state, numberOfCakes: state.numOfCakes - 1 }
+    default: return state
+  }
+}
+
+// Store
+const store = createStore(cakeReducer)
+
 function App () {
   return (
-    <div>
-      <h2>Hello World</h2>
-      <img src={logo512} />
-    </div>
+    <Provider store={store}>
+      <div>
+        <img src={logo512} width='240'/>
+        <h2>Cake Shop</h2>
+        <p>Number of Cakes</p>
+        <br/>
+        <h3></h3>
+      </div>
+    </Provider>
   )
 }
 
