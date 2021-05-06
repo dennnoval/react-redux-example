65a456d8352ecdc5699cc8837ce105d6f91beb0a
dennnoval <dennnoval@gmail.com>
2021-05-06 21:59:38 +0700

    Update files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import logo512 from './res/logo512.png'
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 // Action
 const buyCake = () => {
@@ -11,13 +11,14 @@ const buyCake = () => {
 
 function App () {
   const numberOfCakes = useSelector(state => state.numOfCakes)
+  const dispatch = useDispatch()
   return (
     <div>
       <img src={logo512} width='240'/>
       <h2>Cake Shop</h2>
       <p>Number of Cakes: {numberOfCakes}</p>
       <br/>
-      <button type='button' onClick={() => buyCake()}></button>
+      <button type='button' onClick={() => dispatch(buyCake())}></button>
     </div>
   )
 }
--- src/StateManagement/store.js
+++ src/StateManagement/store.js
@@ -8,7 +8,7 @@ const initialState = {
 // Reducer
 const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'BUY_CAKE': return { ...state, numberOfCakes: state.numOfCakes - 1 }
+    case 'BUY_CAKE': return { ...state, numOfCakes: state.numOfCakes - 1 }
     default: return state
   }
 }
